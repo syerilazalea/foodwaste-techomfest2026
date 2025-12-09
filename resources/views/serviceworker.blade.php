4b1ad05ed438861443030c42254ea9b44867ffbb
Evan <nyxora1@gmail.com>
2025-12-09 18:23:29 +0700

    ntah mboh

--- resources/views/serviceworker.blade.php
+++ resources/views/serviceworker.blade.php
@@ -30,7 +30,13 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request)
-        .then(response => response || fetch(event.request))
-        .catch(() => caches.match('/offline'))
+        .then(response => {
+            return response || fetch(event.request);
+        })
+        .catch(() => {
+            if (event.request.mode === 'navigate') {
+                return caches.match('/offline');
+            }
+        })
     );
 });
